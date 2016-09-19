e0fa4a22b8b3a0e19b072d87af7ad576aaec1e6f
Sebastian Florez <sfrsebastian@gmail.com>
2016-09-18 23:08:54 -0500

    Correcion de referencia

---
+++ Isaacs/Isaacs/HeaderTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HeaderTableViewCell.swift
+//  Isaacs
+//
+//  Created by Sebastian Florez on 9/18/16.
+//  Copyright © 2016 Inspect. All rights reserved.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+
+}
